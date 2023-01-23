fbf0ad2ac5741d126730cb501c166736fb74398d
Ben Madsen <bmadsenonpc@gmail.com>
2023-01-23 14:52:42 -0700

    Fix date and change explore to email

--- explore_dag.py
+++ explore_dag.py
@@ -1,18 +1,23 @@
 from airflow import DAG
 import logging as log
 import pendulum
-from airflow.operators.smooth import SmoothOperator
+from airflow.operators.email import EmailOperator
 from airflow.operators.dummy import DummyOperator
 
 with DAG(
-    dag_id="Smooth_DAG",
+    dag_id="Email_DAG",
     schedule_interval="0 10 * * *",
-    start_date=pendulum.datetime(2023, 9, 1, tz="US/Pacific"),
+    start_date=pendulum.datetime(2023, 1, 20, tz="US/Pacific"),
     catchup=False,
 ) as dag:
     start_task = DummyOperator(task_id="start_task")
-    smooth_task = SmoothOperator()
+    email_task = EmailOperator(
+        task_id="email_task", 
+        to="bmadsenonpc@gmail.com",
+        subject="Someone ran your task",
+        html_content="This email is being sent from your airflow server"
+    )
     end_task = DummyOperator(task_id="end_task")
 
 
-start_task >> smooth_task >> end_task
+start_task >> email_task >> end_task
